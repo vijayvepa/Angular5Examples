4f4ad689ca4ecd49d8ef12d75b55ea129ffb0a9c
Vijay <vijay.k.vepakomma@gmail.com>
2023-08-27 14:28:50 -0400

    route hello to helloworld component

--- CodeUsingJavaPrimeNG/library-management/src/app/app-routing.module.ts
+++ CodeUsingJavaPrimeNG/library-management/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloworldComponent } from './helloworld/helloworld.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'hello', component: HelloworldComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
